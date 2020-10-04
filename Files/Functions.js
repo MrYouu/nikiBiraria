483534fda6544b7cec33826ed28614b0fcf950f1
Jim <51420306+MrYouu@users.noreply.github.com>
2020-10-04 18:09:07 +0300

    Add files via upload

--- Files/Functions.js
+++ Files/Functions.js
@@ -1,1 +1,17 @@
+//  The JavaScript code is property to Mr. You (Kristiyan Valchev) and can only be viewed
+//  If you want to use part of the code, please first contact me: mr.youdevelopment@gmail.com
 
+function openSideMenu()
+{
+    document.getElementById("sideMenu").style.transform = "translateX(0%)";
+}
+
+function closeSideMenu()
+{
+    document.getElementById("sideMenu").style.transform = "translateX(100%)";
+}
+
+function defaultFunction()
+{
+
+}
